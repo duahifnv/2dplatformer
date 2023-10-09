41b4ff3ba3e21dcf72e326145ebdb0fab7a669a5
duahifnv <max135603@gmail.com>
2023-10-09 15:36:49 +0300

    2Д ПЛАТФОРМЕР 1) Добавлена физика персонажа (гравитация, коллизия с полом) 2) Добавлено управление персонажем ("WASD")

--- js/game2d.js
+++ js/game2d.js
@@ -1,72 +1,109 @@
-const canvas = document.getElementById("canvas-2d");
-const ctx = canvas.getContext("2d");
+var canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas-2d"));
+const c = canvas.getContext("2d");
 
-canvas.width = 1500;
-canvas.height = 600;
+canvas.width = 1024;
+canvas.height = 512;
 
-function drawCascade(){
-    const colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple"];
-    for(let i = 0; i < colors.length; i++){
-        let color = colors[i];
-        ctx.beginPath();
-        ctx.rect(
-            i * 45,
-            i * 45,
-            canvas.width - 400,
-            canvas.height - 350
+const playerSize = 50;
+const gravityC = .5;
+
+// Класс управляемого игрока
+class Player {
+    constructor(position, size) {
+        this.position = position
+        this.size = {
+            width: playerSize,
+            height: playerSize
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
+    }
+    draw() {
+        c.fillStyle = "white";
+        c.fillRect(
+            this.position.x,
+            this.position.y,
+            this.size.width,
+            this.size.height
         );
-        ctx.fillStyle = color;
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = color;
-        ctx.lineJoin = "round";
-    
-    
-        ctx.fill();
-        ctx.stroke();
+    }
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+
+        // Гравитация
+        this.position.y += this.velocity.y;
+        if(this.position.y + this.size.height + this.velocity.y > canvas.height) {
+            this.velocity.y = 0;
+            this.position.y += canvas.height - (this.position.y + this.size.height);
+        }
+        else {
+            this.velocity.y += gravityC;
+        }
     }
 }
 
-function drawArc(circleCount, circleRadius){
-    const colors = ["#393939", "#7d7d7d", "#bfbfbf"];
-    const pi = Math.PI;
-    for(let i = 0; i < circleCount; i++){
-        let radius = circleRadius * (i + 1);
-        let angle = pi/4 * i;
-        ctx.beginPath();
-        ctx.arc(
-            canvas.width/2,
-            canvas.height/2,
-            radius,
-            angle,
-            angle + pi/2,
-        );
-        ctx.lineWidth = 10;
-        ctx.fill();
-        ctx.fillStyle = colors[i % 3];
-        ctx.strokeStyle = colors[i % 3];
-        ctx.stroke();
+const player = new Player({
+    x: 500, y: canvas.height - playerSize
+})
+
+// Нажимаемые клавиши
+const keys = {
+    d: {
+        isPressed: false
+    },
+    a: {
+        isPressed: false
+    },
+    w: {
+        isPressed: false
     }
 }
 
-function circleInRect(){
-    const pi = Math.PI;
-    ctx.lineWidth = 10;
-
-    ctx.beginPath();
-    ctx.fillStyle = "blue";
-    ctx.fillRect(100, 100, 200, 200);
-    ctx.strokeRect(100, 100, 200, 200);
-
-    ctx.lineWidth = 3;
-    ctx.arc(200, 200, 70, -pi/4, pi * 1.5);
-    ctx.strokeStyle = "red";
-    ctx.fillStyle = "yellow";
-    ctx.fill();
-    ctx.stroke();
+// Функция постоянной отрисовки поля и игрока
+function animate(){
+    window.requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    player.velocity.x = 0;
+    if (keys.d.isPressed) player.velocity.x = 5;
+    else if (keys.a.isPressed) player.velocity.x = -5;
 }
+animate();
 
-circleInRect();
-
+// Прослушивание нажатых и отжатых клавиш
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.isPressed = true;
+            break;
+        case 'a':
+            keys.a.isPressed = true;
+            break;
+        case 'w':
+            keys.w.isPressed = true;
+            if (player.position.y + player.size.height == canvas.height) {
+                player.velocity.y = -10;
+            }
+            break;
+    }
+});
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            keys.d.isPressed = false;
+            break;
+        case 'a':
+            keys.a.isPressed = false;
+            break;
+        case 'w':
+            keys.w.isPressed = false;
+            break;
+    }
+});
 
 
 
